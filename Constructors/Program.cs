365bef13ab1e23a7fe37fb22d5cb3cb3081822b4
Orçun Yıldırım <orcunyildirim172@gmail.com>
2024-02-18 23:10:07 +0300

    Proje Eklemesi Yapıldı

---
+++ Constructors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() { Id=1,FirstName="Orcun",LastName="Demir",City="Isparta"};
+
+            Customer customer2 = new Customer(2,"Sıla","Güven","Aydın");
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+
+            Console.ReadLine();
+        }
+    }
+
+    class Customer
+    {
+        // default constructor
+        public Customer()
+        {
+            
+        }
+        public Customer(int id, string firstname, string lastname,string city)
+        {
+            FirstName=firstname;
+            LastName=lastname;
+            Id=id;
+            City=city;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Interfaces2/Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+
+namespace Interfaces2
+{
+    class Program
+    {
+        //  interfaces new' lenemez
+        static void Main(string[] args)
+        {
+            //IPersonManager customerManager = new CustomerManager();        
+
+            //IPersonManager employeeManager = new EmployeeManager();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new CustomerManager());
+            projectManager.Add(new InternManager());
+            projectManager.Add(new EmployeeManager());
+
+        }
+    }
+
+
+
+    interface IPersonManager
+    {
+        // unimplemented operation
+        void Add();
+        void Update();
+
+    }
+
+    // Inherits - class -------- implements - interface
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            // müşteri ekleme kodları
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi");
+        }
+    }
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer güncellendi");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+
+
+    }
+
+
+}
